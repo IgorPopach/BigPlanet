e28ca1645f753bbb6d1f4ddc274eabcf8e77f796
Igor Popach <igorpopach@gmail.com>
2019-11-26 18:30:30 +0200

    started new project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button type="button" className="btn btn-primary">
+                    Hello!
+                </button>
+            </header>
+        </div>
+    );
+}
+
+export default App;
